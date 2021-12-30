6e8588833e1168d691dedf440e9ea4a26e6c2c55
nece001 <nece001@163.com>
2021-12-30 14:37:00 +0800

    update databases/DatabaseSeeder.php

--- databases/DatabaseSeeder.php
+++ databases/DatabaseSeeder.php
@@ -16,5 +16,6 @@ class DatabaseSeeder extends Seeder
         // \App\Models\User::factory(10)->create();
         $this->call(AdminRoleMenuTableSeeder::class);
         $this->call(ActivityTypesTableSeeder::class);
+        $this->call(AdminUsersTableSeeder::class);
     }
 }
