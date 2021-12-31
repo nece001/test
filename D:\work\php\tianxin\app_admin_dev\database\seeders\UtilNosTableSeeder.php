39df83b93a80f5314fa82927d55c4897306da4b5
nece001 <nece001@163.com>
2021-12-31 15:35:18 +0800

    create D:\work\php\tianxin\app_admin_dev\database\seeders\UtilNosTableSeeder.php

---
+++ D:\work\php\tianxin\app_admin_dev\database\seeders\UtilNosTableSeeder.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class UtilNosTableSeeder extends Seeder
+{
+
+    /**
+     * Auto generated seed file
+     *
+     * @return void
+     */
+    public function run()
+    {
+        
+
+        \DB::table('util_nos')->delete();
+        
+        \DB::table('util_nos')->insert(array (
+            0 => 
+            array (
+                'id' => 1,
+                'created_at' => '2021-08-05 13:54:24',
+                'updated_at' => '2021-08-05 13:54:24',
+                'deleted_at' => NULL,
+                'util_type_id' => 1,
+                'util_no' => 'util0001',
+                'title' => '工具1',
+                'tenant_id' => 'tenant01',
+                'param' => '{}',
+            ),
+        ));
+        
+        
+    }
+}
